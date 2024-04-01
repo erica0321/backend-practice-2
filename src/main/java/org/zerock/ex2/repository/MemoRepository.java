9fd2cd7ea687db72a4d831437fdf32bc6143b7ec
hyunahrho29@gmail.com <hyunahrho29@gmail.com>
2024-04-01 15:58:21 +0900

    Add query

--- src/main/java/org/zerock/ex2/repository/MemoRepository.java
+++ src/main/java/org/zerock/ex2/repository/MemoRepository.java
@@ -1,9 +1,32 @@
 package org.zerock.ex2.repository;
 
+import jakarta.transaction.Transactional;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.zerock.ex2.entity.Memo;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 
 public interface MemoRepository extends JpaRepository<Memo, Long> {
 
+  List<Memo> findByMnoBetweenOrderByMnoDesc(Long form, Long to);
+
+  Page<Memo> findByMnoBetween(Long form, Long to, Pageable pageable);
+
+  void deleteMemoByMnoLessThan(Long num);
+
+  @Transactional
+  @Modifying
+  @Query("select m from Memo m order by m.mno desc")
+  List<Memo> getListDesc();
+
+  @Transactional
+  @Modifying
+  @Query("update Memo m set m.memoText = :memoText where m.mno = :mno")
+  int updateMemoText(@Param("mno") Long mno, @Param("memoText") String memoText);
 }
--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -7,8 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 import org.zerock.ex2.entity.Memo;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -20,7 +23,7 @@ public class MemoRepositoryTests {
   //등록 테스트
   @Test
 //  @Transactional
-  public void testPageDefault() {
+  public void testInsert() {
     IntStream.rangeClosed(1, 100).forEach(i -> {
       Memo memo =  Memo.builder().memoText("[ " + i + " ]").build();
       memoRepository.save(memo);
@@ -51,5 +54,67 @@ public class MemoRepositoryTests {
   public void testDelete() {
     Long mno = 100L;
     memoRepository.deleteById(mno);
+  }
+
+  //페이징 처리
+  @Test
+  public void testPageDefault() {
+    Pageable pageable = PageRequest.of(0, 10);
+    Page<Memo> result = memoRepository.findAll(pageable);
+    System.out.println(result);
+    System.out.println("--------------------------");
+    System.out.println("Total Pages: " + result.getTotalPages());
+    System.out.println("Total Count: " + result.getTotalElements());
+    System.out.println("Page Number: " + result.getNumber());
+    System.out.println("Page Size: " + result.getSize());
+    System.out.println("has next page? : " + result.hasNext());
+    System.out.println("first page?: " + result.isFirst());
+  }
+
+  //조건 추가
+  @Test
+  public void testSort() {
+    Sort sort1 = Sort.by("mno").descending();
+    Pageable pageable = PageRequest.of(0, 10, sort1);
+    Page<Memo> result = memoRepository.findAll(pageable);
+
+    result.get().forEach(memo -> {System.out.println(memo);});
+  }
+
+  //쿼리 추가
+  @Test
+  public void testQueryMethods() {
+    List<Memo> list = memoRepository.findByMnoBetweenOrderByMnoDesc(70L, 80L);
+    for(Memo memo : list){
+        System.out.println(memo);
+    }
+  }
+
+  @Test
+  public void testQueryMethodWithPageable() {
+    Pageable pageable = PageRequest.of(0, 10, Sort.by("mno").descending());
+    Page<Memo> result = memoRepository.findByMnoBetween(10L, 50L, pageable);
+
+    result.get().forEach(memo -> System.out.println(memo));
+  }
+
+  @Test
+  @Commit
+  @Transactional
+  public void testDeleteQueryMethod() {
+    memoRepository.deleteMemoByMnoLessThan(10L);
+  }
+
+  @Test
+  public void getListDesc(){
+    List<Memo> memo = memoRepository.getListDesc();
+    System.out.println(memo);
+  }
+
+  @Test
+  @Commit
+  @Transactional
+  public void updateMemoText() {
+    int i = memoRepository.updateMemoText(10L, "update by query");
   }
 }
